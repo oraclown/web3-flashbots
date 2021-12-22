69a6f49c1ee38821b03b751f37031be6198ffc0e
oraclown <owen.burton13@gmail.com>
2021-12-22 07:56:55 -0800

    report on testnet with flashbots

--- examples/reporter.py
+++ examples/reporter.py
@@ -9,25 +9,24 @@ from eth_account.signers.local import LocalAccount
 from flashbots import flashbot
 from web3 import Web3, HTTPProvider
 from web3.exceptions import TransactionNotFound
-from web3.types import TxParams
 from dotenv import load_dotenv, find_dotenv
 
-from telliot_core.apps.telliot_config import TelliotConfig
-from telliot_core.contract.contract import Contract
-from telliot_core.queries.legacy_query import LegacyRequest
 from examples.abi import gorli_playground_abi
-# from telliot_feed_examples.utils.log import get_logger
+from flashbots.provider import get_default_endpoint
+from web3.middleware import geth_poa_middleware
+from telliot_feed_examples.feeds.trb_usd_feed import trb_usd_median_feed
+import asyncio
+from flashbots.etherscan_gas import EtherscanGasPriceSource
 
 
 load_dotenv(find_dotenv())
-# logger = get_logger(__name__)
 
 
 def env(key: str) -> str:
     return os.environ.get(key)
 
 
-def main() -> None:
+async def main() -> None:
     # account to send the transfer and sign transactions
     account: LocalAccount = Account.from_key(env("SENDER_PRIVATE_KEY"))
     # account to establish flashbots reputation
@@ -36,73 +35,70 @@ def main() -> None:
     chain_id = 5
 
     w3 = Web3(HTTPProvider(env("PROVIDER")))
-    flashbot(w3, signature, env("FLASHBOTS_HTTP_PROVIDER_URI"))
+    if chain_id == 5:
+        w3.middleware_onion.inject(geth_poa_middleware, layer=0)
+    endpoint = env("FLASHBOTS_HTTP_PROVIDER_URI") if chain_id == 5 else get_default_endpoint()
+    flashbot(w3, signature, endpoint)
 
     print(f"Reporter account balance: {Web3.fromWei(w3.eth.get_balance(account.address), 'ether')} ETH")
 
-    playground = w3.eth.contract(
-        address="0x3477EB82263dabb59AC0CAcE47a61292f28A2eA7",  # Gorli playground addr
-        abi=gorli_playground_abi)
+    playground_contract = w3.eth.contract(
+        address="0x3477EB82263dabb59AC0CAcE47a61292f28A2eA7",  # Gorli contract addr
+        abi=gorli_playground_abi
+    )
+
+    _ = await trb_usd_median_feed.source.fetch_new_datapoint()
+    latest_data = trb_usd_median_feed.source.latest
+    if latest_data[0] is None:
+        msg = "Unable to retrieve updated datafeed value."
+        print(msg)
     
-    query = LegacyRequest(legacy_id=99)
-    nonce_getter = playground.get_function_by_name("getNewValueCountbyQueryId")
+    query = trb_usd_median_feed.query
+    nonce_getter = playground_contract.get_function_by_name("getNewValueCountbyQueryId")
     timestamp_count = nonce_getter(_queryId=query.query_id).call()
     print('timestamp count (nonce):', timestamp_count)
 
-    submit_val_func = playground.get_function_by_name("submitValue")
+    submit_val_func = playground_contract.get_function_by_name("submitValue")
     submit_val_tx = submit_val_func(
         _queryId=query.query_id,
-        _value=query.value_type.encode(420.0),
+        _value=query.value_type.encode(latest_data[0]),
         _nonce=timestamp_count,
         _queryData=query.query_data,
     )
-
     acc_nonce = w3.eth.get_transaction_count(account.address)
+
+    # fetch tip and base fee
+    c = EtherscanGasPriceSource()
+    result = await c.fetch_new_datapoint()
+    next_base_fee = result[0].suggestBaseFee
+    print('next suggested base fee:', next_base_fee)
+    max_priority = result[0].FastGasPrice
+    print('priority fee:', max_priority)
+    max_fee = next_base_fee + max_priority
+    print('max fee:', max_fee)
+
+    gas = 350000
+
     built_submit_val_tx = submit_val_tx.buildTransaction(
         {
             "nonce": acc_nonce,
-            "gas": 350000,
-            "maxFeePerGas": Web3.toWei(200, "gwei"),
-            "maxPriorityFeePerGas": Web3.toWei(50, "gwei"),
+            "gas": gas,
+            "maxFeePerGas": Web3.toWei(max_fee, "gwei"),
+            "maxPriorityFeePerGas": Web3.toWei(max_priority, "gwei"),
             "chainId": chain_id,
         }
     )
     submit_val_tx_signed = account.sign_transaction(built_submit_val_tx)
 
-    # return
-
-    # bundle two EIP-1559 (type 2) transactions, pre-sign one of them
+    # bundle one pre-signed, EIP-1559 (type 2) transaction
     # NOTE: chainId is necessary for all EIP-1559 txns
     # NOTE: nonce is required for signed txns
 
-    # nonce = w3.eth.get_transaction_count(account.address)
-    # tx1: TxParams = {
-    #     "to": receiver.address,
-    #     "value": Web3.toWei(0.01, "ether"),
-    #     "gas": 25000,
-    #     "maxFeePerGas": Web3.toWei(200, "gwei"),
-    #     "maxPriorityFeePerGas": Web3.toWei(50, "gwei"),
-    #     "nonce": nonce,
-    #     "chainId": chain_id,
-    # }
-    # tx1_signed = account.sign_transaction(tx1)
-
-    # tx2: TxParams = {
-    #     "to": receiver.address,
-    #     "value": Web3.toWei(0.01, "ether"),
-    #     "gas": 25000,
-    #     "maxFeePerGas": Web3.toWei(200, "gwei"),
-    #     "maxPriorityFeePerGas": Web3.toWei(50, "gwei"),
-    #     "chainId": chain_id,
-    # }
-
     bundle = [
         {"signed_transaction": submit_val_tx_signed.rawTransaction},
-        # {"signed_transaction": tx1_signed.rawTransaction},
-        # {"signer": sender, "transaction": tx2},
     ]
 
-    # send bundle to be executed in the next 5 blocks
+    # send bundle to be executed in the next blocks
     block = w3.eth.block_number
     results = []
     for target_block in [block + k for k in [1, 2, 3, 4, 5]]:
@@ -122,4 +118,4 @@ def main() -> None:
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
